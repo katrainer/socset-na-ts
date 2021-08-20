e865e2c9b1499fc2f25b1973a05fc555a2637918
katrainer <laub94@list.ru>
2021-08-20 14:32:39 +0300

    socset-na-ts
    socset-na-ts

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://com-x.life/templates/Default/dleimages/noavatar.png" alt="batman"/>
+        </header>
+    )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://get.wallhere.com/photo/sunlight-trees-landscape-sunset-lake-nature-reflection-grass-sky-field-sunrise-green-morning-Sun-panorama-Pennsylvania-marsh-creek-state-park-dawn-agriculture-meadow-prairie-5760x1080-px-rural-area-atmospheric-phenomenon-computer-wallpaper-573802.jpg"
+                    alt="aaa"/>
+            </div>
+            <div>
+                avatar-descript
+            </div>
+            <div>
+                my post
+                <div>
+                    New post
+                </div>
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
+    )
+}
