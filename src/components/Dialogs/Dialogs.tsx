28971a64d5ef980c3fec6c195d80a31181f286b0
katrainer <laub94@list.ru>
2021-09-08 21:48:27 +0300

    lesson 23
    add data and id: v1()

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
 import {Dialog} from './Dialog/Dialog';
 import classes from "./Dialogs.module.css";
 import {Message} from "./Message/Message";
+import {v1} from "uuid";
 
 export const Dialogs = () => {
+
+    let dialogsData=[
+        {id:v1(), name: 'Nikita'},
+        {id:v1(), name: 'Jana'},
+        {id:v1(), name: 'Daniil'},
+        {id:v1(), name: 'Lecha'}
+    ]
+
+    let messagesData=[
+        {id:v1(), message: 'yo'},
+        {id:v1(), message: 'yoyo'},
+        {id:v1(), message: 'yoyoyo'},
+        {id:v1(), message: 'yoyoyoyo'}
+    ]
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItem}>
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import {Post} from "./Post/Post";
 import s from "./MyPost.module.css"
+import {v1} from "uuid";
 
 type postType = {}
 
 export const MyPosts = (props: postType) => {
+
+    let postsData=[
+        {id:v1(), message: 'yo', likeCount:12},
+        {id:v1(), message: 'yoyo', likeCount: 212},
+      ]
+
     return (
         <div className={s.postBlock}>
             <h3>My posts</h3>
