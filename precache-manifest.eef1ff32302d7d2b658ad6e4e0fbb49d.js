172835e597ed49def499bf8b09eb166e1f73007c
Katrain <laub94@list.ru>
2022-05-05 20:29:42 +0300

    Updates

---
+++ precache-manifest.eef1ff32302d7d2b658ad6e4e0fbb49d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651e874da4613375b99d3b80e80e19de",
+    "url": "/socset-na-ts/index.html"
+  },
+  {
+    "revision": "2968c98b280359f90fe8",
+    "url": "/socset-na-ts/static/css/main.a83e2dc4.chunk.css"
+  },
+  {
+    "revision": "6e8ce6d4d58632deeb6a",
+    "url": "/socset-na-ts/static/js/2.223a63e5.chunk.js"
+  },
+  {
+    "revision": "72b690856a7e8e2f3f1a",
+    "url": "/socset-na-ts/static/js/3.5d44fe4c.chunk.js"
+  },
+  {
+    "revision": "2968c98b280359f90fe8",
+    "url": "/socset-na-ts/static/js/main.83b0d7c0.chunk.js"
+  },
+  {
+    "revision": "a60b9b763f13b959ca81",
+    "url": "/socset-na-ts/static/js/runtime~main.cd7f31e3.js"
+  },
+  {
+    "revision": "087f5d2ffaf77dec5696b81ca52c63de",
+    "url": "/socset-na-ts/static/media/preloader.087f5d2f.gif"
+  }
+]);
